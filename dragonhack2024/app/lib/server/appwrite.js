64b798b6fc85952c86891dd53532bcb90f52a2a7
Đorđe <xs55755@student.uni-lj.si>
2024-04-20 23:06:54 +0200

    Added getters for Users, Tutors and Notes

--- dragonhack2024/app/lib/server/appwrite.js
+++ dragonhack2024/app/lib/server/appwrite.js
@@ -10,4 +10,60 @@ client
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_URL)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
 
-const databse = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
+const database = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
+const collectionUsers = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_USERS_ID;
+const collectionNotes = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_NOTES_ID;
+const bucketProofs = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_PROOFS_ID;
+const bucketPfp = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_PFP_ID;
+const bucketNotes = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_NOTES_ID;
+
+
+// Actions for setting up and gettings users. 
+
+export async function getUser (auth0Id) {
+    // Query for user by auht0Id
+    noStore();
+    const data = await databases.listDocuments(
+        database, 
+        collectionUsers, 
+        [
+            Query.orderDesc("$createdAt"),
+            Query.equal("user_id_Auth0", auth0Id)
+        ]
+    );
+
+    return data;
+}
+
+// Actions for getting tuuors
+
+export async function listTutors() {
+    noStore();
+    const data = await databases.listDocuments(
+        database,
+        collectionUsers,
+        [
+            Query.orderDesc("$createdAt"),
+            Query.equal("isTutor", "true")
+        ]
+    );
+
+    return data;
+}
+
+// Actions for getting notes
+
+export async function getNotes() {
+  noStore();
+  const data = await databases.listDocuments(
+    database,
+    collectionNotes,
+    [
+      Query.orderDesc("$createdAt"),
+    ]
+  );
+
+  return data;
+}
+
+
