6408d9ae2d0c5c50e87fe1d52d336d37693894e2
Đorđe <xs55755@student.uni-lj.si>
2024-04-20 20:16:25 +0200

    Povezano, enterAs je prebaceno na '/'.

--- dragonhack2024/components/Button.tsx
+++ dragonhack2024/components/Button.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react'
 import Image from 'next/image';
 
@@ -6,13 +8,21 @@ type ButtonProps = {
     title: string;
     icon?: string;
     variant?: string;
+    href?: string;
 }
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, href }: ButtonProps) => {
   return (
     <button 
-    className={`flexCenter gap-3 rounded-full border ${variant}`}
-    type={type}>
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      type={type}
+      onClick={()=>{
+        if(href){
+          window.location.href = href
+          console.log(href);
+        }
+      }}
+    >
         {icon && <Image src={icon} alt={title} width={24} height={24} />}
         <label className="bold-16 whitespace-nowrap">{title}</label>
     </button>
--- dragonhack2024/components/Hero.tsx
+++ dragonhack2024/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Button from "@/components/Button"
+// import Button from "@/components/Button"
+import Link from 'next/link'
 
 const Hero = () => {
   return (
@@ -7,20 +8,10 @@ const Hero = () => {
     <h1 className="text-4xl md:text-6xl font-bold mb-8">TUTORIFY</h1>
     <div className="flex flex-col gap-4 md:flex-row md:gap-8">
         <div className="lg:flexCenter hidden">
-            <Button
-                type="button"
-                title="Login"
-                icon="/user.svg"
-                variant="btn_dark_green"
-            /> 
+            <Link href="/api/auth/login" > Log in </Link> 
         </div>
         <div className="lg:flexCenter hidden">
-            <Button
-                type="button"
-                title="Login"
-                icon="/user.svg"
-                variant="btn_green"
-            /> 
+            <Link href="/api/auth/login"> Sign up </Link>
         </div>
     </div>
   </div>
